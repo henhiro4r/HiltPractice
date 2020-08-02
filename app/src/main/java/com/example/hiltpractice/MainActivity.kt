e615c9848f731ed7b1b23d1f870bce00619399d2
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2020-08-03 05:19:47 +0700

    updates

--- app/src/main/java/com/example/hiltpractice/MainActivity.kt
+++ app/src/main/java/com/example/hiltpractice/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.hiltpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ApplicationComponent
 import javax.inject.Inject
+import javax.inject.Qualifier
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,28 +25,64 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println(someClass.doAThing())
-        println(someClass.doSomething())
+        println(someClass.doAThing2())
     }
 }
 
 class SomeClass
 @Inject
 constructor(
-    private val someOtherClass: SomeOtherClass
+    @Impl1 private val someInterfaceImpl: SomeInterface,
+    @Impl2 private val someInterfaceImpl2: SomeInterface
 ) {
-    fun doAThing() : String{
-        return "Some string"
+    fun doAThing(): String {
+        return "Look :${someInterfaceImpl.getSomething()}"
     }
 
-    fun doSomething(): String{
-        return someOtherClass.doSomething()
+    fun doAThing2(): String {
+        return "Look :${someInterfaceImpl2.getSomething()}"
     }
 }
 
-class SomeOtherClass
+class SomeInterfaceImpl
 @Inject
-constructor() {
-    fun doSomething(): String{
-        return "Look another string"
+constructor(): SomeInterface{
+    override fun getSomething(): String {
+        return "Something1"
     }
 }
+
+class SomeInterfaceImpl2
+@Inject
+constructor(): SomeInterface{
+    override fun getSomething(): String {
+        return "Something2"
+    }
+}
+
+interface SomeInterface {
+    fun getSomething(): String
+}
+
+@InstallIn(ApplicationComponent::class)
+@Module
+class MyModule{
+    @Impl1
+    @Singleton
+    @Provides
+    fun provideSomeInterface(): SomeInterface{
+        return SomeInterfaceImpl()
+    }
+
+    @Impl2
+    @Singleton
+    @Provides
+    fun provideSomeInterface2(): SomeInterface{
+        return SomeInterfaceImpl2()
+    }
+}
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Impl1
+annotation class Impl2
